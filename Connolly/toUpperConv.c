3abdec636a415fab1937f290403d8b79cfa0d3e3
Kieran Connolly <KieranConnolly@users.noreply.github.com>
2015-01-28 22:31:59 -0600

    Update toUpperConv.c

--- Connolly/toUpperConv.c
+++ Connolly/toUpperConv.c
@@ -19,17 +19,17 @@ int main(int argc, char * argv[])
   //scanf("%s", &line1);
   argv[0] = line1;
   printf("line1 is %s", line1);
-  fileinput = fopen(line1, "r"); //Uses string accepted from user.            
-  
+  fileinput = fopen(line1, "r");             
+  fileoutput = fopen(argv[0], "w");
 
-  
-  int a, b;
-  a = getchar();
-  while (a != 'EOF')
+  int ltr1, ltr2;
+  ltr1 = getchar();
+  while (ltr1 != EOF)
     {
-      if ((isupper a) == 0)
-        {tolower a;
-        fprintf ('a');
+      if ((isupper (ltr1)) == 0)
+        {
+          tolower (ltr1);
+          fprintf (fileoutput, "%u", ltr1);
         }
     }
   return 0;
