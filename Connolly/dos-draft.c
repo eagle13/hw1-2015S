943551d0c2bb1574b832d75fe6a1fa9b27e219e4
Kieran Connolly <KieranConnolly@users.noreply.github.com>
2015-01-28 22:38:12 -0600

    Create dos-draft.c

---
+++ Connolly/dos-draft.c
@@ -0,0 +1,26 @@
+#include <stdio>
+#include <string>
+
+int main()
+{
+  //Change the source to read input
+
+  //getline
+  //Increment through a line, keeping track of the current character and the character behind it.
+  //Once the first character = \n, delete the character behind it!
+  
+  //getchar() = char a 
+  while (a != EOF)
+  {
+    while (a != '\n')
+    {   
+      char b = a;
+      getchar() = a;
+      }
+   } 
+    //delete char b
+
+   */
+  return 0;
+
+}
